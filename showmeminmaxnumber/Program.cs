05e3f89a75dc6e3bbee59421d5e854e61f46d308
연우 이 <lyw9114@gmail.com>
2020-05-12 12:17:17 +0900

    초기 커밋.

---
+++ showmeminmaxnumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace showmeminmaxnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                string num = Console.ReadLine();
+                array[i] = int.Parse(num);
+            }
+            Console.WriteLine("--------------------------------------");
+
+            int max=array[0], min=array[0];
+
+            for(int i=1; i<5; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+                else if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine("가장 큰 수 : " + max);
+            Console.WriteLine("가장 작은 수 : " + min);
+        }
+    }
+}
